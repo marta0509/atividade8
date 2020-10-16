9a58c70182b91806f5316f40e4617faf363bd435
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 12:21:59 +0100

    Update dois.php and um.php

---
+++ dois.php
@@ -0,0 +1,7 @@
+<?php
+
+for ($i=1; $i<=20; $i++){
+	$nota =rand(1,20);
+	echo 'A nota do aluno <span style="color:#0000cc">' .$i. '</span> é <span style="color:#0000cc">' .$nota. '</span><br>';
+}
+?>
---
+++ um.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i=1; $i<=20 ; $i++) { 
+	
+	echo "Aluno Número " .$i;
+	echo "<br>";
+}
+
+echo "<br>";
+
+for ($i=20; $i>1; $i--){ 
+	
+	echo "Aluno Número " .$i;
+	echo "<br>";
+}
+?>
